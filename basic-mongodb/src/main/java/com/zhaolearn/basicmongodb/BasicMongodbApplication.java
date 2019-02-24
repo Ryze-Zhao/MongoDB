96032936aea79bb285c87a001460a33c98b7d3e4
RyzeZhao <643080112@qq.com>
2019-02-24 16:52:16 +0800

    MongoDB：Basic-MongoDB模块:初始化

---
+++ basic-mongodb/src/main/java/com/zhaolearn/basicmongodb/BasicMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.zhaolearn.basicmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicMongodbApplication.class, args);
+    }
+
+}
