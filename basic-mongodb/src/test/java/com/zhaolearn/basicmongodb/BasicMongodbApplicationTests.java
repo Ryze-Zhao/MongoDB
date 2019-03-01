4f8fea4dad98d0bcdeab27aee695272810282e44
RyzeZhao <643080112@qq.com>
2019-03-01 11:23:56 +0800

    MongoDB：Basic-MongoDB模块:测试类测试findAll()

--- basic-mongodb/src/test/java/com/zhaolearn/basicmongodb/BasicMongodbApplicationTests.java
+++ basic-mongodb/src/test/java/com/zhaolearn/basicmongodb/BasicMongodbApplicationTests.java
@@ -2,15 +2,33 @@ package com.zhaolearn.basicmongodb;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicMongodbApplicationTests {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private UseMongoTemplate useMongoTemplate;
+    @Autowired
+    private PeopleServiceImpl peopleService;
 
     @Test
-    public void contextLoads() {
+    public void findAll() {
+        List<People> peopleList=peopleService.findAll();
+        System.out.println("Repository：");
+        peopleList.stream().forEach(e->System.out.println(e.toString()));
+        List<People> peopleList1=useMongoTemplate.findAll();
+        System.out.println("Template：");
+        peopleList1.stream().forEach(e->System.out.println(e.toString()));
     }
+
+
 
 }
