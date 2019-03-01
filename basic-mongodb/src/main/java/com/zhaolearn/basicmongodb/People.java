1b38020c08da90e7ff85643fc29ec260a9a74dcd
RyzeZhao <643080112@qq.com>
2019-03-01 10:56:09 +0800

    MongoDB：Basic-MongoDB模块:实体类创建

---
+++ basic-mongodb/src/main/java/com/zhaolearn/basicmongodb/People.java
@@ -0,0 +1,22 @@
+package com.zhaolearn.basicmongodb;
+
+public class People {
+    //MongoDB中id使用的是_id，会自动转换为id，不需要下横杠
+    private String id;
+    private String name;
+    private String age;
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getAge() { return age; }
+    public void setAge(String age) { this.age = age; }
+    @Override
+    public String toString() {
+        return "People{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
